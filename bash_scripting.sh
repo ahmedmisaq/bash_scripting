3cb95525598325764f58a8e692266bd604b13fa5
Ahmed Isaq <ahmed.isaq@nbcuni.com>
2026-02-21 03:10:31 +0000

    Using bash to echo variables

---
+++ bash_scripting.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name="Ahmed"
+age=34
+occupation="Sr Ops Analyst"
+
+echo "My name is $name and I am $age years old and I am a $occupation"
+
+
+# Prints greeting line to the console
+
+: ' This is a multi
+line comment ' 
