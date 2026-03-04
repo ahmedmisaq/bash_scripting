92490d5f6271f07484659d1a240a8783a12959af
Ahmed Isaq <aisaq@hotmail.co.uk>
2026-03-04 02:07:51 +0000

    Implement  to show argument count, script name, and passed parameters

---
+++ func_par.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+print_args() {
+    echo "Number of Arguments $#"
+    echo "Script name $0"
+    echo "First Argument $1"
+    echo "Second Argument $2"
+    echo "All Arguments $@"
+}
+
+
+# print_args "Ahmed" "Muhammed" "Muhammed" "Isaq"
+
+print_args "Ahmed" "Bahja"
