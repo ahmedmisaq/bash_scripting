eb3f75173d40ef3a60048abcab8e5420ecfc02db
Ahmed Isaq <aisaq@hotmail.co.uk>
2026-03-16 17:51:20 +0000

    Add bash script to display common environment variables

---
+++ standard_env_var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Below are some commonly used standard environment variables.
+
+echo "Username: $LOGNAME"
+echo "Current Directory: $PWD"
+echo "Executable Paths $PATH"
+echo "Default Language: $LANG"
